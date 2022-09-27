db37f1521132da69efc9d5b334c84afa0dfa7345
helio-lopes13 <heliosgod13@hotmail.com>
2022-09-27 19:10:44 -0300

    Commit oficial do projeto concluído

---
+++ podcastr-react/src/App.js
@@ -0,0 +1,15 @@
+import Button from "./Button";
+
+function App() {
+  return (
+    <>
+      <Button><h1>Button 1</h1></Button>
+      <Button><h2>Button 2</h2></Button>
+      <Button><h3>Button 3</h3></Button>
+      <Button><h4>Button 4</h4></Button>
+      <Button><h5>Button 5</h5></Button>
+    </>
+  );
+}
+
+export default App;
